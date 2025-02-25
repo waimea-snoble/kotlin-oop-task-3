03bed45b5b7348bfce28bc9ab769a5062d1840ea
Steve Copley <steve.copley@waimea.school.nz>
2025-02-26 00:14:29 +1300

    Updated tasks

--- README.md
+++ README.md
@@ -14,11 +14,8 @@ __{__}__   /\
 ```
 
 Gnomes are placed on the steps in the garden. The top step is #1, the lowest step is #10
+
 - Only one gnome can be on a step at a time
-- Gnomes can be shifted to different steps,
-  but only if they are unoccupied
-- Gnomes can be moved up and down the steps,
-  but not above the top step (1), nor below the
-  lowest (10)
-- If Gnomes are moving up/down, and the target
-  step is occupied, they take the step before
+- Gnomes can be shifted to different steps, but only if they are unoccupied
+- Gnomes can be moved up and down the steps, but not above the top step (1), nor below the lowest (10)
+- If Gnomes are moving up/down, and the target step is occupied, they take the step before
--- src/Main.kt
+++ src/Main.kt
@@ -14,13 +14,13 @@
  *                    #3  |
  *
  * Gnomes are placed on the steps in the garden.
- * The top step is #1, the lowest step is #10
+ * The top step is #1, the lowest step is #5
  * - Only one gnome can be on a step at a time
  * - Gnomes can be shifted to different steps,
  *   but only if they are unoccupied
  * - Gnomes can be moved up and down the steps,
  *   but not above the top step (1), nor below the
- *   lowest (10)
+ *   lowest (5)
  * - If Gnomes are moving up/down, and the target
  *   step is occupied, they take the step before
  * ================================================
@@ -32,21 +32,88 @@ fun main() {
     println("------------------------")
 
     // Instantiate steps
-
     val steps = Steps()
+    steps.show()
 
+    check(steps.gnomeCount() == 0)
+
+    println("------------------------")
     // Instantiate gnomes
-
     val jim = Gnome("Jim")
     val sam = Gnome("Sam")
     val amy = Gnome("Amy")
 
-    check(jim.stepNumber(steps) == 0)
-    steps.placeGnomeOnStep(jim, 2)
-    steps.placeGnomeOnStep(sam, 4)
-    steps.placeGnomeOnStep(amy, 8)
-
+    println("------------------------")
+    // Place gnomes on steps
+    steps.placeGnome(1, jim)    // Should be fine
+    steps.placeGnome(4, sam)
     steps.show()
+
+    check(steps.gnomeCount() == 2)
+
+    check(steps.stepNumOfGnome(jim) == 1)
+    check(steps.stepNumOfGnome(sam) == 4)
+    check(steps.stepNumOfGnome(amy) == 0)          // Not placed yet
+
+    check(steps.gnomeOnStep(1) == jim)
+    check(steps.gnomeOnStep(4) == sam)
+    check(steps.gnomeOnStep(5) == null)   // Empty step
+
+    println("------------------------")
+    // Try to place gnomes off the steps
+    steps.placeGnome(6, amy)    // Should do nothing
+    steps.show()
+    steps.placeGnome(0, amy)    // Should do nothing
+    steps.show()
+
+    check(steps.gnomeCount() == 2)
+    check(steps.stepNumOfGnome(amy) == 0)
+
+    println("------------------------")
+    // Try to place a gnome on an occupied step
+    steps.placeGnome(1, amy)    // Should swap out Jim
+    steps.show()
+
+    check(steps.gnomeCount() == 2)
+    check(steps.stepNumOfGnome(amy) == 1)
+    check(steps.stepNumOfGnome(jim) == 0)
+
+    println("------------------------")
+    // Try to clear a gnome from a step
+    steps.clearStep(4)          // Should remove Sam
+    steps.show()
+
+    check(steps.gnomeCount() == 1)
+    check(steps.stepNumOfGnome(sam) == 0)
+
+    println("------------------------")
+    // Try to remove a specific gnome
+    steps.removeGnome(amy)          // Should remove it
+    steps.show()
+
+    check(steps.gnomeCount() == 0)
+    check(steps.stepNumOfGnome(amy) == 0)
+
+    println("------------------------")
+    // Try to remove a specific gnome not on steps
+    steps.removeGnome(amy)          // Should do nothing as not on steps
+    steps.show()
+
+    println("------------------------")
+    // Try to place a gnome that is already on steps
+    steps.placeGnome(3, jim)    // Should place it ok
+    steps.show()
+
+    steps.placeGnome(5, jim)    // Should move it down
+    steps.show()
+
+    steps.placeGnome(5, jim)    // Should do nothing
+    steps.show()
+
+    check(steps.gnomeCount() == 1)
+    check(steps.stepNumOfGnome(jim) == 5)
+
+
 }
 
 
@@ -57,17 +124,12 @@ class Steps() {
     // Steps list, either holding a gnome or null
     val steps = mutableListOf<Gnome?>()
 
+    val STEPCOUNT = 5
+
     init {
         println("Setting up steps...")
-        // 10 empty steps
-        for (i in 1..10) steps.add(null)
-    }
-
-    /**
-     * Place a given gnome on the given step (1-10)
-     */
-    fun placeGnomeOnStep(gnome: Gnome, step: Int) {
-
+        // Add empty steps
+        for (i in 1..STEPCOUNT) steps.add(null)
     }
 
     /**
@@ -79,27 +141,51 @@ class Steps() {
      * etc.
      */
     fun show() {
-        // Loop thru steps, getting the index and gnome
-        for ((i, gnome) in steps.withIndex()) {
 
-        }
+    }
+
+    /**
+     * Return the number of gnomes on steps
+     */
+    fun gnomeCount(): Int {
+        return 0
+    }
+
+    /**
+     * Place a given gnome on the given step (1-5)
+     */
+    fun placeGnome(step: Int, gnome: Gnome) {
 
     }
 
     /**
      * Find a given gnome on the steps. Return the
-     * step number (1-10) if found, or 0 if not
+     * step number (1-5) if found, or 0 if not
      */
-    fun findGnome(gnome: Gnome): Int {
+    fun stepNumOfGnome(gnome: Gnome): Int {
         return 0
     }
 
     /**
-     * Return the gnome on the given step (1-10),
-     * or null if empty
+     * Return the gnome on the given step (1-5),
+     * or null if out of range or empty
      */
-    fun getGnomeOnStep(stepNum: Int): Gnome? {
+    fun gnomeOnStep(stepNum: Int): Gnome? {
         return null
+    }
+
+    /**
+     * Clear any gnomes off the given step (1-5)
+     */
+    fun clearStep(step: Int) {
+
+    }
+
+    /**
+     * Remove a given gnome from the steps
+     */
+    fun removeGnome(gnome: Gnome) {
+
     }
 }
 
@@ -110,15 +196,6 @@ class Steps() {
 class Gnome(val name: String) {
     init {
         println("Creating a gnome... $name")
-    }
-
-    /**
-     * Given a list of steps, return the step
-     * number (1-10) of this gnome, or 0 if
-     * the gnome is not on the steps
-     */
-    fun stepNumber(steps: Steps): Int {
-        return 0
     }
 }
 
